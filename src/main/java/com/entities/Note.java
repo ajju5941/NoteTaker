eeebe1cdfd79622b160e0548fd885526ae01488c
ajju5941 <aijazahmed.93@gmail.com>
2021-10-12 15:21:40 +0530

    Note Taker App or ToDo App

---
+++ src/main/java/com/entities/Note.java
@@ -0,0 +1,55 @@
+package com.entities;
+
+import java.util.Date;
+import java.util.Random;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="notes")
+public class Note {
+	@Id
+	private int id;
+	private String title;
+	private String content;
+	private Date addDate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getAddDate() {
+		return addDate;
+	}
+	public void setAddDate(Date addDate) {
+		this.addDate = addDate;
+	}
+	public Note(String title, String content, Date addDate) {
+		super();
+		this.id = new Random().nextInt(100000);
+		this.title = title;
+		this.content = content;
+		this.addDate = addDate;
+	}
+	public Note() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
