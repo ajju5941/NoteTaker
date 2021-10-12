a86844537a90153205e964678f93c18634cc69a9
ajju5941 <aijazahmed.93@gmail.com>
2021-10-12 15:42:07 +0530

    Creating FactoryProvider as Singleton Class

--- src/main/java/com/helper/FactoryProvider.java
+++ src/main/java/com/helper/FactoryProvider.java
@@ -5,7 +5,25 @@ import org.hibernate.cfg.Configuration;
 
 public class FactoryProvider {
 	
-
+	private static SessionFactory factory;
+	
+	public static SessionFactory getSessionFactory()
+	{
+		if(factory== null)
+		{
+			factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		}
+	
+		return factory;
+	}
+	
+	public void closeFactory()
+	{
+		if(factory.isOpen())
+		{
+			factory.close();
+		}
+	}
 	
 
 }
